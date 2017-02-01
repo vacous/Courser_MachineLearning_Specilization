f1fd2cf0d255a309e9c4fb635a1df632de509bf7
vacous <vaocus@gmail.com>
2017-02-01 00:21:12 -0500

    till 02

--- PandaTutorial/tutorial.py
+++ PandaTutorial/tutorial.py
@@ -6,18 +6,51 @@ Created on Wed Dec 14 08:06:36 2016
 """
 
 import pandas as pd
-import numpy as np
+import datetime 
+import pandas.io.data as web 
 import matplotlib.pyplot as plt 
-from sklearn import  linear_model
+from matplotlib import style
+import numpy as np
 
-file_resource  = "D:/NetDrive/OneDrive/Coursera/MachineLearning/Uwash-Specilization/02/week_01/data/kc_house_data.csv"
-data = pd.read_csv(file_resource)
-# get the header names for the data frame 
-print "The header names are:"
-print list(data)
-model = linear_model.LinearRegression()
-price = pd.DataFrame(data['price'])
-area = pd.DataFrame(data['sqft_living'])
-model.fit(area,price)
-print model.predict(pd.DataFrame([1,2,3,4]))
+"""
+Example 01 
+"""
+# stock price plot 
+#style.use('ggplot')
+#start = datetime.datetime(2010,1,1)
+#end = datetime.datetime(2015,1,1)
+#df = web.DataReader("XOM", "yahoo", start, end)
+#print(df.head())
+#df['Adj Close'].plot()
+#plt.show()
+
+"""
+Example 02 
+"""
+# data frame = dictionary 
+web_stats = {'day': [1,2,3,4,5,6],'visitors':[43,44,45,46,47,48],'Bounce_rate': [1,2,3,4,5,6]}
+df = pd.DataFrame(web_stats)
+#print(df)
+#print(df.head())
+#print(df.tail())
+##print(df.tail(2))
+#print(df.set_index('day')) # return a new dataframe 
+#df = df.set_index('day')
+## or
+#df.set_index('day', inplace = True)           
+#print(df.head())
+
+## reference specific column 
+print(df['visitors'])
+print(df.visitors) # same 
+print(df[['visitors','Bounce_rate']]) # print two columns 
+
+test = df[['visitors','Bounce_rate']]
+test = np.array(test) # convert to np.array 
+print(test) 
+print(pd.DataFrame(test)) # convert back 
+
+"""
+Example 03 
+"""
 
